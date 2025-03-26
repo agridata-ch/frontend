42ab7b495d1e0a0bd33362a7fe465482f4fa5ba2
Samuel Imboden <samuel.imboden@isceco.admin.ch>
2025-03-26 08:04:57 +0100

    Minor change to test pipeline

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,6 +27,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello ');
+    expect(compiled.querySelector('h1')?.textContent).toContain('agridata.ch');
   });
 });
