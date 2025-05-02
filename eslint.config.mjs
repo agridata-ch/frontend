47e2cde235eba393e1062fedd2f0aac785336609
Philipp Schafer <philipp.schafer@isceco.admin.ch>
2025-05-02 15:24:59 +0200

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
+  {
+    ignores: ['**/commitlint.config.js', '**/temp.js', 'config/*', 'node_modules/*', 'coverage/*'],
+  },
+  tseslint.configs.recommended,
+]);
