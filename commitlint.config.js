99e24bf6cf2c538cd0230eb4d14ac4b533a9a8ea
Philipp Schafer <philipp.schafer@isceco.admin.ch>
2025-05-13 14:05:03 +0200

    feat(setup): update releaserc
    use conventionalcommits as preset for release notes generator
    
    DIGIB2-297
    
    chore(DIGIB2-297): update releaserc
    
    feat(DIGIB2-297): improve buttons for filters

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,24 +2,5 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always'], // or [0, 'always'] to disable entirely
-    'jira-ticket': [2, 'always'], // Add this line to enforce the rule (2 = error)
   },
-  plugins: [
-    {
-      rules: {
-        // Custom rule for checking Jira tickets in the commit message
-        'jira-ticket': ({ scope }) => {
-          if (/^release$/.test(scope)) return [true]; // Allow 'release' scope without Jira ticket
-          // Check if the commit message scope matches the Jira ticket pattern
-          if (!/DIGIB2-\d+/.test(scope)) {
-            return [
-              false,
-              'Commit message must include a Jira ticket in the format DIGIB2-<ticket-number>',
-            ];
-          }
-          return [true];
-        },
-      },
-    },
-  ],
 };
