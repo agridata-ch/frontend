dba25147196c5a5bde423d43e30d464131b88737
Philipp Schafer <philipp.schafer@isceco.admin.ch>
2025-06-04 09:15:17 +0200

    chore(auth): add custom login guard
    chore(auth): add custom login guard

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,7 +5,7 @@ export const environment = {
     authority: 'https://idp-rf.agate.ch/auth/realms/agate',
     clientId: 'agridata-rf',
     redirectUrl: 'https://dev.agridata.ch/auth-response',
-    postLogoutRedirectUri: 'https://dev.agridata.ch/auth-response',
+    postLogoutRedirectUri: 'https://dev.agridata.ch',
     scope: 'openid',
     responseType: 'code',
   },
