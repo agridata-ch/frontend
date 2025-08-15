788fe20ecf8add3631f62a23e24d545f200d6578
Samuel Imboden <samuel.imboden@isceco.admin.ch>
2025-08-15 13:07:53 +0200

    feat(cicd): improve pipelines for multiple environments DIGIB2-709

---
+++ src/environments/environment.integration.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://api.int.agridata.ch',
+  oidc: {
+    authority: 'https://idp-rf.agate.ch/auth/realms/agate',
+    clientId: 'agridata-rf',
+    redirectUrl: 'https://int.agridata.ch/auth-response',
+    postLogoutRedirectUri: 'https://int.agridata.ch',
+    scope: 'openid offline_access',
+    responseType: 'code',
+    silentRenew: true,
+    useRefreshToken: true,
+    secureRoutes: ['https://api.int.agridata.ch'],
+  },
+};
