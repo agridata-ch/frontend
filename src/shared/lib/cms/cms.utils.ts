aabd1a3801aaa71dbec176b70fe357f8afac7d14
Philipp Schafer <philipp.schafer@isceco.admin.ch>
2025-08-20 13:57:12 +0200

    chore(cms): fix media url for dev and prod

--- src/shared/lib/cms/cms.utils.ts
+++ src/shared/lib/cms/cms.utils.ts
@@ -2,6 +2,9 @@ import { environment } from '@/environments/environment';
 import { VIDEO_FORMATS } from '@/shared/constants/constants';
 
 export const generateMediaUrl = (url: string) => {
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url;
+  }
   return `${environment.cmsBaseUrl}${url}`;
 };
 
