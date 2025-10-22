acd6a5b670ee481c6cde2b9de9d361aba651aeef
Philipp Schafer <philipp.schafer@isceco.admin.ch>
2025-10-22 14:25:13 +0200

    fix(authentication): fix logout
    remove unused code for cross tab sync which causes logout issues

--- src/app/layout/default-layout.component.ts
+++ src/app/layout/default-layout.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AuthService } from '@/shared/lib/auth';
 import { ToastComponent } from '@/shared/ui/toast';
 import { FooterWidgetComponent } from '@/widgets/footer-widget';
 import { HeaderWidgetComponent } from '@/widgets/header-widget';
@@ -28,19 +27,4 @@ import { SupporterOverlayComponent } from '@/widgets/supporter-overlay/supporter
   ],
   templateUrl: './default-layout.component.html',
 })
-export class DefaultLayoutComponent {
-  private readonly authService = inject(AuthService);
-
-  constructor() {
-    // Refresh the page when the authentication state changes
-    // This is useful for cases where the user logs in or out in a different tab
-    let prev = this.authService.isAuthenticated();
-    effect(() => {
-      const curr = this.authService.isAuthenticated();
-      if (prev && !curr) {
-        window.location.reload();
-      }
-      prev = curr;
-    });
-  }
-}
+export class DefaultLayoutComponent {}
