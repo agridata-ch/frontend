bfb0db556dfc3726770adfe36816417170f779d6
Samuel Imboden <samuel.imboden@isceco.admin.ch>
2026-03-16 12:20:13 +0100

    fix(cache): fix caching issue by adding query param to i18n assets

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@jsverse/transloco';
+
+import { version as frontendVersion } from '../../package.json';
 
 /**
  * Implements a loader for fetching translation JSON files from the assets folder. It integrates with the Transloco
@@ -13,6 +15,10 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   private readonly http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang.toLowerCase()}.json`);
+    return this.http.get<Translation>(
+      `/assets/i18n/${lang.toLowerCase()}.json?v=${frontendVersion}`,
+      /* A version query parameter is appended to the request URL to prevent browser caching issues
+         when a new frontend version is deployed. This ensures updated translation files are always fetched. */
+    );
   }
 }
