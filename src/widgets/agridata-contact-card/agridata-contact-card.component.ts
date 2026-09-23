64631a04458a0e30247d311f28a31201bf5e8caf
Jana Stojanovic <jana.stojanovic@blw.admin.ch>
2026-09-23 10:30:16 +0200

    feat(consent-request): add foreign badge and alert for international consumers
    DIGIB2-1659

--- src/widgets/agridata-contact-card/agridata-contact-card.component.ts
+++ src/widgets/agridata-contact-card/agridata-contact-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, input } from '@angular/core';
+import { faGlobe } from '@awesome.me/kit-0b6d1ed528/icons/classic/regular';
 
+import { I18nPipe } from '@/shared/i18n';
 import { AgridataAvatarComponent, AvatarSize, AvatarSkin } from '@/shared/ui/agridata-avatar';
+import { AgridataBadgeComponent, BadgeSize } from '@/shared/ui/badge';
 
 /**
  * Displays an avatar and some contact information
@@ -9,13 +12,19 @@ import { AgridataAvatarComponent, AvatarSize, AvatarSkin } from '@/shared/ui/agr
  */
 @Component({
   selector: 'app-agridata-contact-card',
-  imports: [AgridataAvatarComponent],
+  imports: [AgridataAvatarComponent, AgridataBadgeComponent, I18nPipe],
   templateUrl: './agridata-contact-card.component.html',
 })
 export class AgridataContactCardComponent {
+  // Constants
+  protected readonly BadgeSize = BadgeSize;
+  protected readonly iconGlobe = faGlobe;
+
+  // Input properties
   readonly name = input<string | undefined>();
   readonly secondaryName = input<string | undefined>();
   readonly imageUrl = input<string | undefined>();
   readonly size = input<AvatarSize>(AvatarSize.LARGE);
   readonly skin = input<AvatarSkin>(AvatarSkin.DEFAULT);
+  readonly showForeignBadge = input<boolean>(false);
 }
